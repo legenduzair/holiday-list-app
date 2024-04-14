b5bcb2a3740796b0d5fe68cb3ea5887d355e73b4
Uzair Oomer <uzair.oomer@jdplc.com>
2024-04-14 16:00:33 +0100

    add key to svg element to remove console error

--- src/components/Rating/StarRating.tsx
+++ src/components/Rating/StarRating.tsx
@@ -8,8 +8,9 @@ type ratingProps = {
 const StarRating = ({ rating }: ratingProps) => {
   return (
     <div className="ratings">
-      {[...Array(rating)].map(() => (
+      {[...Array(rating)].map((_, index) => (
         <svg
+        key={index}
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 24 24"
           fill="currentColor"
